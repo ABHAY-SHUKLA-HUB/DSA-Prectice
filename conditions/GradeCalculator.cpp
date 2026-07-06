e3f371e96da7dbbc9f89a29965462ab405e05d96
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-07-06 18:04:59 +0530

    Grade Calculator

---
+++ conditions/GradeCalculator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Input Marks: ";
+    cin>>n;
+
+    if(n<=100 && n>=90){
+        cout<<"A";
+    }
+    else if(n<=89 && n>=80){
+        cout<<"B";
+    }
+    else if(n<=79 && n>=70){
+        cout<<"C";
+    }
+    else if(n<=69 && n>=60){
+        cout<<"D";
+    }
+    else{
+        cout<<"Fail";
+    }
+    return 0;
+}
