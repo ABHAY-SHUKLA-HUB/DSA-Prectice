451e3281c603e33363b958efac95443be1893cdd
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-07-07 11:55:07 +0530

    Write a program to find the factorial of a number.

--- Loops/Factorial.cpp
+++ Loops/Factorial.cpp
@@ -1,13 +1,14 @@
-// Sum of first N natural numbers
+// Write a program to find the factorial of a number.
+
 
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
-    int sum=0;
+    int fact=1;
     for(int i=1;i<=n;i++){
-        sum=sum+i;
+        fact*=i;
     }
-    cout<<sum;
+    cout<<fact;
 }
