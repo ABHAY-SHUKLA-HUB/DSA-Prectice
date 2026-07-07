cb133aa8bb384fb79686e34f11d1753bb99d1407
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-07-07 11:22:30 +0530

    Print all even numbers from 1 to N.

---
+++ Loops/PrintallEvenNumbersfrom1toN.cpp
@@ -0,0 +1,12 @@
+//Print all even numbers from 1 to N.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=2; i<=n;i+=2){
+        cout<<i<<" ";
+    }
+    return 0;
+}
