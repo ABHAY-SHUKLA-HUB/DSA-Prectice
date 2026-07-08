cd89e58333842e62d9acdea54f4932bb2e298772
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-07-08 10:30:58 +0530

    Product of Digits

--- Loops/ProductofDigits.cpp
+++ Loops/ProductofDigits.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int sum=0;
+    int product=1;
     while(n!=0){
-        sum = sum + n%10;
+        product*=(n%10);
         n=n/10;
     }
-    cout<<sum;
+    cout<<product;
+
     return 0;
 }
