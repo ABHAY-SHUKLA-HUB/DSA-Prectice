9277ccd4488317029110712c93ccb84b6500ae13
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-07-09 18:56:29 +0530

    Right-Aligned-half-pyramid

---
+++ Nested-Loops/Right-AlignedHalfPyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        // Print stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
