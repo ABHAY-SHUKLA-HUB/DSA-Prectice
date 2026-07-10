bbb5f2ed042d1ee91375a849d75e4db2f3cc706d
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-07-10 18:15:34 +0530

    Hollow-Diamond

---
+++ Nested-Loops/Hollow-Diamond.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            if(j==1 || j==2*i-1 || i==n){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<=i-1;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*(n-i)+1;j++){
+            if( j==1 || j==2*(n-i)+1){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
