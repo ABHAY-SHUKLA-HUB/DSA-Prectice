a85b16bd7ade70decf9abd50ef4d080e2956d260
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-07-24 19:17:56 +0530

    08-Reverse-Array

---
+++ Arrays/08-Reverse-Array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number of array: ";
+    cin>>n;
+    int arr[100];
+    for(int i=0;i<n;i++){
+        cout<<"Array: ";
+        cin>>arr[i];
+    }
+    int start=0;
+    int end=n-1;
+    while(start<end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
